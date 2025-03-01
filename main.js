45317bbf2616d05ad8a9e1104fe9cafb99f9b34f
Shukurullo2012 <shukur.ubaydullaev909@gmail.com>
2025-03-01 10:49:40 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+let rang = prompt("Rang kiriting");
+let textcolor = prompt("textni colorini kiriting");
+
+let newrang = document.createElement("p");
+document.body.style.backgroundColor = rang;
+document.body.append(newrang)
+
+let newtextcolor = document.createElement("p")
+document.body.style.color = textcolor;
+document.body.append(newtextcolor);
+
+
+
+// const box = document.querySelector(".korobka  ")
+// console.log(box)
+// box.innerHTML
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
